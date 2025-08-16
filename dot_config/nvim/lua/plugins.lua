4b9e1688614cf251c5d6ece41336c3ac6f261eae
Silvio Luiz <1054587+silvioluiz@users.noreply.github.com>
2025-08-16 01:04:42 -0300

    Correcao carregamento de plugins

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -44,38 +44,47 @@ return {
   },
 
   -- LSP (Mason + LSPConfig com auto-setup)
-  { "neovim/nvim-lspconfig" },
-  { "williamboman/mason.nvim", build = ":MasonUpdate" },
-  { "williamboman/mason-lspconfig.nvim" },
   {
-    -- bloco de config que liga tudo do LSP
-    "_lsp-setup",
-    lazy = false,
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
     config = function()
+      -- Ativa o mason, que gerencia os LSPs
       require('mason').setup()
-      require('mason-lspconfig').setup({ ensure_installed = {} })
+
+      -- Ativa o mason-lspconfig, que faz a ponte entre mason e lspconfig
+      require('mason-lspconfig').setup({
+        -- Lista de servidores para garantir que estejam instalados.
+        -- Deixe vazio para instalar sob demanda, ou adicione servidores. Ex: { "lua_ls", "pyright" }
+        ensure_installed = {},
+      })
 
       local lspconfig = require('lspconfig')
+
+      -- Função a ser executada quando um servidor LSP se anexa a um buffer
       local on_attach = function(_, bufnr)
         local function bufmap(mode, lhs, rhs, desc)
           vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
         end
-        bufmap('n','gd', vim.lsp.buf.definition, 'Goto Definition')
-        bufmap('n','K',  vim.lsp.buf.hover,      'Hover')
-        bufmap('n','gr', vim.lsp.buf.references, 'References')
-        bufmap('n','<leader>rn', vim.lsp.buf.rename, 'Rename')
+        -- Mapeamentos de atalhos do LSP
+        bufmap('n', 'gd', vim.lsp.buf.definition, 'Goto Definition')
+        bufmap('n', 'K',  vim.lsp.buf.hover,      'Hover')
+        bufmap('n', 'gr', vim.lsp.buf.references, 'References')
+        bufmap('n', '<leader>rn', vim.lsp.buf.rename, 'Rename')
+        bufmap('n', '<leader>ca', vim.lsp.buf.code_action, 'Code Action')
       end
 
-      -- Auto-configura qualquer servidor instalado via :Mason
+      -- Configura automaticamente os servidores instalados pelo Mason
+      -- para usar as configurações definidas acima (on_attach, etc)
       require('mason-lspconfig').setup_handlers({
-        function(server)
-          lspconfig[server].setup({ on_attach = on_attach })
+        function(server_name) -- A configuração padrão
+          lspconfig[server_name].setup({
+            on_attach = on_attach,
+          })
         end,
       })
-
-      -- Exemplos (ative se quiser fixar):
-      -- lspconfig.lua_ls.setup({ on_attach = on_attach })
-      -- lspconfig.pyright.setup({ on_attach = on_attach })
     end,
   },
 }
